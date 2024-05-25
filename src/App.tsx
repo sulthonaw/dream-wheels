ff2b1d22377d415674c5eefee57e35262255afc5
sulthonaw <sultona412@gmail.com>
2024-05-25 21:54:46 +0700

    fix: bug comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AboutUs from "./sections/about-us/AboutUs";
 import Catalog from "./sections/catalog/Catalog";
 import CTA from "./sections/cta/CTA";
 import Footer from "./sections/footer/Footer";
-import Header from "./sections/header/Header";
+// import Header from "./sections/header/Header";
 import Hero from "./sections/hero/Hero";
 import Testimonials from "./sections/testimonials/Testimonials";
 
